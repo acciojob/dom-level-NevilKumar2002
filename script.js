04c90d4a8f73a35ef13c10dd1fbb4833c750b6b9
NevilKumar2002 <127580266+NevilKumar2002@users.noreply.github.com>
2023-08-29 07:49:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+// script.js
+
+// Get a reference to the element with id="level"
+var targetElement = document.getElementById("level");
+
+// Initialize the level count to 0
+var levelCount = 1;
+
+// Traverse up the DOM tree until the top-most parent (html element)
+while (targetElement !== null && targetElement.parentElement !== null) {
+    targetElement = targetElement.parentElement;
+    levelCount++;
+}
+
+// Display the DOM level using alert
+alert("The level of the element is: " + levelCount);
